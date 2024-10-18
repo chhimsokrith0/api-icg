9514cdfc0ecd4585b011d9fdf2e692a6c648cfa2
chhimsokrith <sokrithchhim@gmail.com>
2024-10-18 19:32:51 +0700

    fff

--- config/db.js
+++ config/db.js
@@ -10,13 +10,23 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME   // MySQL database name from .env
 });
 
-// Connect to MySQL
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   }
+// });
+
 db.connect((err) => {
   if (err) {
-    console.error('MySQL connection error:', err); // Log connection errors
-    throw err;
+    console.error('Error connecting to the database:', err.stack);
+    return;
   }
-  console.log('MySQL Connected...'); // Log success message when connected
+  console.log('Connected to the MySQL database');
 });
 
 // Export the connection for use in other parts of the application
